505e057011b17a480de71fb0e6ffc659d233181b
Hongming Zhu <zhm1019@qq.com>
2024-07-24 13:08:52 +0800

    quick setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Set Up Your PC Quickly
+
+## References
+
+* [Beautiful Bash](https://www.youtube.com/watch?v=b3W7Ky_aaaY)
+
---
+++ git/config-git.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+git config --global user.email "zhm1019@qq.com"
+git config --global user.name "Hongming Zhu"
+git config --global core.editor "vim"
+
