51496d6f3fc3db21c4a111780cbf4aa5b289ced8
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-09-30 00:22:01 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,25 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt-get install -y net-tools git
+sudo apt-get install -y net-tools git openssh-server
 
 # clash
 # https://nyanpasu.elaina.moe/tutorial/install.html
+
+# ssh-keygen
+# .ssh/
+
+
+sudo apt-get install -y help2man perl python3 make
+sudo apt-get install -y g++  # Alternatively, clang
+sudo apt-get install -y libgz  # Non-Ubuntu (ignore if gives error)
+sudo apt-get install -y libfl2  # Ubuntu only (ignore if gives error)
+sudo apt-get install -y libfl-dev  # Ubuntu only (ignore if gives error)
+sudo apt-get install -y zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
+
+sudo apt-get install -y ccache  # If present at build, needed for run
+sudo apt-get install -y mold  # If present at build, needed for run
+sudo apt-get install -y libgoogle-perftools-dev numactl
+
+sudo apt-get install -y perl-doc
+sudo apt-get install -y autoconf flex bison
