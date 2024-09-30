d1b323360a47f81199a2b6dc954473010fcb58bf
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-09-30 11:10:48 +0800

    Create install-cmake.sh

---
+++ cmake/install-cmake.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+wget -q https://github.com/Kitware/CMake/releases/download/v3.30.4/cmake-3.30.4.tar.gz
+tar -xvf cmake-3.30.4.tar.gz
+cd cmake-3.30.4
+./bootstrap && make && sudo make install
+cd ..
+rm -rf cmake-3.30.4 cmake-3.30.4.tar.gz
