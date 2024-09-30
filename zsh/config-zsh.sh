b97ae0b4b0553c2dcee3f58b649f2985608bba9e
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-09-30 10:42:40 +0800

    Create config-zsh.sh

---
+++ zsh/config-zsh.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+## https://gist.github.com/luizomf/f0aad4db02e1ee4fdce2769ac58122fb
+
+sudo apt install zsh -y
+sudo apt-get install powerline fonts-powerline -y
+git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+
+# REBOOT
+# sudo reboot
+
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
+git clone git@github.com:denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+git clone git@github.com:zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone git@github.com:dracula/gnome-terminal ~/.gnome-terminal
+cd ~/.gnome-terminal
+./install.sh
+cd ~
+rm -Rf ~/.gnome-terminal
+
+# USE .zshrc config from
+# https://gist.github.com/luizomf/35aa0596ea45555286d3f70133afebcc
+
+# SET BACKGROUND COLOR TO
+# #171724
