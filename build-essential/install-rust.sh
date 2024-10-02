32a06da8196575e09be053679612bcbe733f5d37
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-10-02 23:41:18 +0800

    Create install-rust.sh

---
+++ build-essential/install-rust.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+## https://www.rust-lang.org/tools/install
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.zshrc
