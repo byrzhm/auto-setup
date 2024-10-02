635a342e275024ce635d49ecd3a638998eecdc66
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-10-02 23:02:08 +0800

    Create install-glow.sh

---
+++ command-line-tools/install-glow.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+## https://github.com/charmbracelet/glow
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
