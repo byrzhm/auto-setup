37301ee8e697decae51e5b79b07b0d45e39bb13e
doublesevenshop <92587816+doublesevenshop@users.noreply.github.com>
2024-10-02 11:55:35 +0800

    Update install-cmake.sh
    -_-

--- cmake/install-cmake.sh
+++ cmake/install-cmake.sh
@@ -3,6 +3,6 @@
 wget -q https://github.com/Kitware/CMake/releases/download/v3.30.4/cmake-3.30.4.tar.gz
 tar -xvf cmake-3.30.4.tar.gz
 cd cmake-3.30.4
-./bootstrap && make && sudo make install
+./bootstrap && make -j$(nproc) && sudo make install
 cd ..
 rm -rf cmake-3.30.4 cmake-3.30.4.tar.gz
