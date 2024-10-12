0d06db41b3fa676f57e4d43b0ee15b36f8a97964
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-10-12 23:15:01 +0800

    Create install-nodejs.sh

---
+++ build-essential/install-nodejs.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+## https://github.com/nvm-sh/nvm
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
