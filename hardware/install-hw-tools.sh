5bab89516d798bc7f2602e92edbd9a089353a475
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-10-16 18:33:29 +0800

    Update install-hw-tools.sh

--- hardware/install-hw-tools.sh
+++ hardware/install-hw-tools.sh
@@ -1,9 +1,43 @@
 #! /bin/bash
 
-## gtkwave iverlog verilator
+## iverlog gtkwave verilator
 
-sudo apt install -y gtkwave iverilog
+## iverilog
+git clone git@github.com:steveicarus/iverilog.git --depth=1
+cd iverilog
+./configure --prefix=$HOME/.local
+make -j"$(nproc)"
+sudo make install
 
+## gtkwave
+### gtkwave3
+sudo apt install libbz2-dev liblzma-dev # build-essential
+sudo apt install libgtk-3-dev automake # gperf
+sudo apt install tcl-dev tk-dev
+sudo apt install libjudy-dev # optional
+
+git clone git@github.com:gtkwave/gtkwave.git -b lts gtkwave --depth=1
+cd gtkwave/gtkwave3-gtk3 && ./autogen.sh
+./configure --enable-gtk3 --enable-judy --prefix=$HOME/.local
+make -j"$(nproc)"
+sudo make install
+
+
+
+### gtkwave4
+sudo apt install libbz2-dev # build-essential gperf meson
+sudo apt install desktop-file-utils libgtk-3-dev # flex git
+sudo apt install libgirepository1.0-dev
+sudo apt install libjudy-dev # optional
+
+git clone git@github.com:gtkwave/gtkwave.git --depth=1
+cd gtkwave
+meson setup build --prefix=$HOME/.local
+meson compile -C build
+sudo meson install -C build
+
+
+## verilator
 export VERILATOR_INSTALL_PATH="$HOME/tmp/verilator"
 git clone git@github.com:verilator/verilator.git "$VERILATOR_INSTALL_PATH"
 cd "$VERILATOR_INSTALL_PATH"
@@ -13,3 +47,12 @@ make -j `nproc`  # Build Verilator itself (if error, try just 'make')
 sudo make install
 
 # cd .. && rm -rf "$VERILATOR_INSTALL_PATH"
+
+
+## expect
+wget https://sourceforge.net/projects/expect/files/latest/download -O expect.tar.gz
+tar -xzf expect.tar.gz
+cd expect*
+./configure --prefix=$HOME/.local
+make -j"$(nproc)"
+make install
