492089c3f1de53405a84d937b327fd4bdb9f252b
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-10-31 15:08:16 +0800

    Update config-tmux.sh

--- tmux/config-tmux.sh
+++ tmux/config-tmux.sh
@@ -7,3 +7,5 @@ sudo apt install -y tmux
 echo "set -g mouse on" > ~/.tmux.conf
 echo "bind - split-window -v" >> ~/.tmux.conf
 echo "set -g default-terminal \"screen-256color\"" >> ~/.tmux.conf
+
+## More configuration, see https://github.com/tmux/tmux/wiki/Getting-Started
