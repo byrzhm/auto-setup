b8a7d42a510e16aa2fb4efb2115c02a763f3437e
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2024-12-27 10:44:58 +0800

    Update install-nodejs.sh

--- build-essential/install-nodejs.sh
+++ build-essential/install-nodejs.sh
@@ -8,3 +8,5 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 exec $SHELL
 nvm install stable
 nvm use stable
+
+npm config set registry https://mirrors.cloud.tencent.com/npm/
