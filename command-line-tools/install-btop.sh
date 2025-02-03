cba714b36b126727091d3f396a5749bf6b4864c8
Hongming Zhu <92535690+byrzhm@users.noreply.github.com>
2025-02-03 18:09:57 +0800

    Update install-btop.sh

--- command-line-tools/install-btop.sh
+++ command-line-tools/install-btop.sh
@@ -4,6 +4,7 @@
 wget https://github.com/aristocratos/btop/releases/download/v1.4.0/btop-x86_64-linux-musl.tbz
 tar -xvjf btop-x86_64-linux-musl.tbz
 cd btop
+# make install PREFIX=~/.local
 sudo make install
 cd ..
 rm -rf btop btop-x86_64-linux-musl.tbz
